5575f1ce152e71d715cfca2c7176f52a4907e882
Lele Liu <fj12342005@126.com>
2026-03-06 10:11:01 +0100

    .

--- assets/js/navigate.js
+++ assets/js/navigate.js
@@ -1,26 +1,8 @@
 
 $(document).ready(function() {
-    
-    $("#nav_1").click(function() {
-        var root_path = get_root_path();
-        location.href = root_path;
-    });
-
-    $("#nav_2").click(function() {
-        var root_path = get_root_path();
-        location.href = root_path + "/publications";
-    });
-
     initializeSubtitleSidebar();
     initializeMobileSidebarToggle();
 });
-
-function get_root_path() {
-    var full_path = window.document.location.href;
-    var words = full_path.split('/');
-    var root_path = words[0] + '//' + words[2];
-    return root_path;
-}
 
 function initializeSubtitleSidebar() {
     var subtitleList = document.getElementById("subtitle-list");
